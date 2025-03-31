a74f9fb131a81b5fd38abb7093b5496a16029a71
Vincents13 <6327.vincent@gmail.com>
2025-03-31 17:56:29 +0700

    sistem gerak

---
+++ src/BuildableObject.java
@@ -0,0 +1,9 @@
+public class BuildableObject{
+    float position,rotation;
+    //kudu e onok image gawe gambar peletakane
+
+    public void render(){
+        // gawe render gamabar enek wis 
+    }
+   
+}
