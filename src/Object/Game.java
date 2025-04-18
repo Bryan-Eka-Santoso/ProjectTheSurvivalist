c43e64e074ee1d30b033ab1943b18d9d2bc929a7
Zinx140 <frostocean4@gmail.com>
2025-04-18 23:19:36 +0700

    ayam ngendog

--- src/Object/Game.java
+++ src/Object/Game.java
@@ -24,112 +24,112 @@ public class Game extends JPanel implements KeyListener {
     int posX = 0;
     int posY = 0;
 
-    public Game() {
+    // public Game() {
         
-        Island island = new Island();
-        Player player = new Player("Player1", island);
+    //     Island island = new Island();
+    //     Player player = new Player("Player1", island);
         
-        JLabel label = new JLabel("Status Bar");
-        label.setBounds(10, 10, 100, 30);
-        label.setForeground(Color.BLACK);
-        label.setFont(new Font("Arial", Font.BOLD, 16));
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        label.setBackground(Color.LIGHT_GRAY);
-        label.setOpaque(true);
-        label.setText(player.displayStats(player));
+    //     JLabel label = new JLabel("Status Bar");
+    //     label.setBounds(10, 10, 100, 30);
+    //     label.setForeground(Color.BLACK);
+    //     label.setFont(new Font("Arial", Font.BOLD, 16));
+    //     label.setHorizontalAlignment(SwingConstants.CENTER);
+    //     label.setVerticalAlignment(SwingConstants.CENTER);
+    //     label.setBackground(Color.LIGHT_GRAY);
+    //     label.setOpaque(true);
+    //     label.setText(player.displayStats(player));
         
-        JPanel inventoryPanel = new JPanel();
-        inventoryPanel.setBackground(Color.LIGHT_GRAY);
-        inventoryPanel.setBounds(125, 550, 800, 100);
+    //     JPanel inventoryPanel = new JPanel();
+    //     inventoryPanel.setBackground(Color.LIGHT_GRAY);
+    //     inventoryPanel.setBounds(125, 550, 800, 100);
 
-        JPanel statusBar = new JPanel();
-        statusBar.setBackground(Color.LIGHT_GRAY);
-        statusBar.setBounds(10, 10, 200, 150);
-        statusBar.add(label);
+    //     JPanel statusBar = new JPanel();
+    //     statusBar.setBackground(Color.LIGHT_GRAY);
+    //     statusBar.setBounds(10, 10, 200, 150);
+    //     statusBar.add(label);
         
-        JPanel inventory = new JPanel();
-        inventory.setBackground(Color.YELLOW);
-        inventory.setBounds(0, 0, 1080, 720);
-        inventory.setLayout(null);
+    //     JPanel inventory = new JPanel();
+    //     inventory.setBackground(Color.YELLOW);
+    //     inventory.setBounds(0, 0, 1080, 720);
+    //     inventory.setLayout(null);
         
-        DrawingPanel gamePanel = new DrawingPanel();
-        gamePanel.setBackground(Color.BLUE);
-        gamePanel.setBounds(posX, posY, WIDTH, HEIGHT);
-        gamePanel.setLayout(null);
+    //     DrawingPanel gamePanel = new DrawingPanel();
+    //     gamePanel.setBackground(Color.BLUE);
+    //     gamePanel.setBounds(posX, posY, WIDTH, HEIGHT);
+    //     gamePanel.setLayout(null);
 
-        Rectangle playerRect = new Rectangle(500, 310, 50, 50);
-        Rectangle enemyRect = new Rectangle(550, 510, 50, 50);
-        gamePanel.addRectangle(playerRect);
-        gamePanel.addRectangle(enemyRect);
+    //     Rectangle playerRect = new Rectangle(500, 310, 50, 50);
+    //     Rectangle enemyRect = new Rectangle(550, 510, 50, 50);
+    //     gamePanel.addRectangle(playerRect);
+    //     gamePanel.addRectangle(enemyRect);
 
-        JPanel maiPanel = new JPanel();
-        maiPanel.setBackground(Color.BLUE);
-        maiPanel.setBounds(0, 0, 1080, 720);
-        maiPanel.setLayout(null);
+    //     JPanel maiPanel = new JPanel();
+    //     maiPanel.setBackground(Color.BLUE);
+    //     maiPanel.setBounds(0, 0, 1080, 720);
+    //     maiPanel.setLayout(null);
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1080, 720);
-        frame.setTitle("The Survivalist Game");
-        frame.setVisible(true);
-        frame.setLayout(null);
-        frame.setResizable(false);
+    //     JFrame frame = new JFrame();
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     frame.setSize(1080, 720);
+    //     frame.setTitle("The Survivalist Game");
+    //     frame.setVisible(true);
+    //     frame.setLayout(null);
+    //     frame.setResizable(false);
         
-        boolean isInvOpen = false;
-        maiPanel.add(statusBar);
-        maiPanel.add(inventoryPanel);
-        maiPanel.add(gamePanel);
+    //     boolean isInvOpen = false;
+    //     maiPanel.add(statusBar);
+    //     maiPanel.add(inventoryPanel);
+    //     maiPanel.add(gamePanel);
 
-        while (true) {
-            if (key == 'i') {
-                isInvOpen = !isInvOpen;
-                System.out.println("Inventory Opened: " + isInvOpen);
-                key = ' ';
-            } 
-            if (key == 'w' && posY - SPEED >= 0) {
-                // playerRect.setBounds(posX, posY - SPEED, 50, 50);
-                gamePanel.setBounds(posX, posY - SPEED, WIDTH, HEIGHT);
-                System.out.println("Player moved up");
-                key = ' ';
-            } 
-            if (key == 's' && posY + SPEED <= HEIGHT) {
-                // playerRect.setBounds(posX, posY + SPEED, 50, 50);
-                gamePanel.setBounds(posX, posY + SPEED, WIDTH, HEIGHT);
-                System.out.println("Player moved down");
-                key = ' ';
-            } 
-            if (key == 'a' && posX - SPEED >= 0) {
-                // playerRect.setBounds(posX - SPEED, posY, 50, 50);
-                gamePanel.setBounds(posX - SPEED, posY, WIDTH, HEIGHT);
-                System.out.println("Player moved left");
-                key = ' ';
-            } 
-            if (key == 'd' && posX + SPEED <= WIDTH) {
-                // playerRect.setBounds(posX + SPEED, posY, 50, 50);
-                gamePanel.setBounds(posX + SPEED, posY, WIDTH, HEIGHT);
-                System.out.println("Player moved right");
-                key = ' ';
-            }
-            if (isInvOpen) {
-                inventoryPanel.setVisible(false);
-                inventory.setVisible(true);
-                gamePanel.setVisible(false);
-                maiPanel.setVisible(false);
-                frame.add(inventory);
-                frame.remove(maiPanel);
-            } else {
-                gamePanel.setVisible(true);
-                inventoryPanel.setVisible(true);
-                inventory.setVisible(false);
-                maiPanel.setVisible(true);
-                frame.remove(inventory);
-            }
+    //     while (true) {
+    //         if (key == 'i') {
+    //             isInvOpen = !isInvOpen;
+    //             System.out.println("Inventory Opened: " + isInvOpen);
+    //             key = ' ';
+    //         } 
+    //         if (key == 'w' && posY - SPEED >= 0) {
+    //             // playerRect.setBounds(posX, posY - SPEED, 50, 50);
+    //             gamePanel.setBounds(posX, posY - SPEED, WIDTH, HEIGHT);
+    //             System.out.println("Player moved up");
+    //             key = ' ';
+    //         } 
+    //         if (key == 's' && posY + SPEED <= HEIGHT) {
+    //             // playerRect.setBounds(posX, posY + SPEED, 50, 50);
+    //             gamePanel.setBounds(posX, posY + SPEED, WIDTH, HEIGHT);
+    //             System.out.println("Player moved down");
+    //             key = ' ';
+    //         } 
+    //         if (key == 'a' && posX - SPEED >= 0) {
+    //             // playerRect.setBounds(posX - SPEED, posY, 50, 50);
+    //             gamePanel.setBounds(posX - SPEED, posY, WIDTH, HEIGHT);
+    //             System.out.println("Player moved left");
+    //             key = ' ';
+    //         } 
+    //         if (key == 'd' && posX + SPEED <= WIDTH) {
+    //             // playerRect.setBounds(posX + SPEED, posY, 50, 50);
+    //             gamePanel.setBounds(posX + SPEED, posY, WIDTH, HEIGHT);
+    //             System.out.println("Player moved right");
+    //             key = ' ';
+    //         }
+    //         if (isInvOpen) {
+    //             inventoryPanel.setVisible(false);
+    //             inventory.setVisible(true);
+    //             gamePanel.setVisible(false);
+    //             maiPanel.setVisible(false);
+    //             frame.add(inventory);
+    //             frame.remove(maiPanel);
+    //         } else {
+    //             gamePanel.setVisible(true);
+    //             inventoryPanel.setVisible(true);
+    //             inventory.setVisible(false);
+    //             maiPanel.setVisible(true);
+    //             frame.remove(inventory);
+    //         }
 
-            frame.addKeyListener(this);
-            frame.add(maiPanel);
-        }
-    }
+    //         frame.addKeyListener(this);
+    //         frame.add(maiPanel);
+    //     }
+    // }
 
     // Method dipanggil saat tombol ditekan
     public void keyPressed(KeyEvent e) {
@@ -175,7 +175,15 @@ public class Game extends JPanel implements KeyListener {
         craftingTable.showRecipes();
         player.inventory.showInventory();
         boolean isRunning = true;
+
+        //buat test doang
+        KandangAyam kandang = new KandangAyam("Chicken Coop", 7, 7);
+        island.buildings.add(kandang);
+        island.world[7][7] = '='; 
+        //======================================
+
         while (isRunning) {
+            // player.displayStats(player);
             player.displayStats(player);
             player.island.showWorld(player, tiger);
             System.out.println("Use WASD to move, Q to quit.");
@@ -206,18 +214,32 @@ public class Game extends JPanel implements KeyListener {
                     craftingTable.craft(player, itemName);
                     break;
                 case "e":
-                    player.useItem();
+                    handleInteraction(player);//useitem jadi satu di procedure ini
                     break;
                 case "h":
                     System.out.println("Enter the index of the item to use: ");
                     int index = getInt.nextInt();
                     player.selectItem(index - 1);
+                case "g":
+                    player.handleGrabAction();
+                    break;
+                
                 default:
                     System.out.println("Invalid input! Use WASD to move or Q to quit.");
                     break;
             }
-            tiger.chasePrey(player); // Tiger chases the player if nearby
+            // tiger.chasePrey(player); // Tiger chases the player if nearby
         }
 
     }
+    public void handleInteraction(Player player) {
+        KandangAyam nearbyKandang = player.findNearbyKandang();
+        if (nearbyKandang != null) {
+            
+            nearbyKandang.interact(player, player.island);
+        } else {
+            player.useItem();
+        }
+    }
+
 }
