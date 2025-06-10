ab4217d719101423fc80f55f1b61ab96b688d4ed
MatthApril <matthew.aprilian@gmail.com>
2025-06-10 20:31:59 +0700

    added and fixed some stuff

--- src/Objek/Items/StackableItem/Foods/Fruits/Guava.java
+++ src/Objek/Items/StackableItem/Foods/Fruits/Guava.java
@@ -7,6 +7,8 @@ import Objek.Player.Player;
 public class Guava extends Food implements Hydrating {
     public Guava(int currentStack) {
         super("Guava", currentStack);
+        this.isCanSell = true;
+        this.price = 5; // Set the price for Guava
         try {
             this.img = javax.imageio.ImageIO.read(new java.io.File("ProjectTheSurvivalist/res/Items/Foods/guava.png")); 
         } catch (java.io.IOException e) {
